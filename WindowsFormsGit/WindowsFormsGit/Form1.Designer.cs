8205d26efe20cf76e25e317665439fc1410fdd6b
karpov <karpov_ak17@nuwm.edu.ua>
2020-05-24 18:17:56 +0300

    зміни карпов

--- WindowsFormsGit/WindowsFormsGit/Form1.Designer.cs
+++ WindowsFormsGit/WindowsFormsGit/Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.kunderenko_KN31_5 = new System.Windows.Forms.Button();
             this.KN_11INT_3 = new System.Windows.Forms.Button();
             this.Khmelnychyi_KN_31 = new System.Windows.Forms.Button();
+            this.karpov_kn_31 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(331, 20);
+            this.button1.Location = new System.Drawing.Point(331, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(159, 51);
             this.button1.TabIndex = 0;
@@ -47,7 +48,7 @@
             // kunderenko_KN31_5
             // 
             this.kunderenko_KN31_5.Location = new System.Drawing.Point(331, 94);
-            this.kunderenko_KN31_5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.kunderenko_KN31_5.Margin = new System.Windows.Forms.Padding(2);
             this.kunderenko_KN31_5.Name = "kunderenko_KN31_5";
             this.kunderenko_KN31_5.Size = new System.Drawing.Size(159, 51);
             this.kunderenko_KN31_5.TabIndex = 1;
@@ -58,7 +59,7 @@
             // KN_11INT_3
             // 
             this.KN_11INT_3.Location = new System.Drawing.Point(331, 167);
-            this.KN_11INT_3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.KN_11INT_3.Margin = new System.Windows.Forms.Padding(2);
             this.KN_11INT_3.Name = "KN_11INT_3";
             this.KN_11INT_3.Size = new System.Drawing.Size(159, 51);
             this.KN_11INT_3.TabIndex = 2;
@@ -76,11 +77,22 @@
             this.Khmelnychyi_KN_31.UseVisualStyleBackColor = true;
             this.Khmelnychyi_KN_31.Click += new System.EventHandler(this.Khmelnychyi_KN_31_Click);
             // 
+            // karpov_kn_31
+            // 
+            this.karpov_kn_31.Location = new System.Drawing.Point(331, 307);
+            this.karpov_kn_31.Name = "karpov_kn_31";
+            this.karpov_kn_31.Size = new System.Drawing.Size(159, 51);
+            this.karpov_kn_31.TabIndex = 4;
+            this.karpov_kn_31.Text = "Карпов";
+            this.karpov_kn_31.UseVisualStyleBackColor = true;
+            this.karpov_kn_31.Click += new System.EventHandler(this.karpov_kn_31_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.karpov_kn_31);
             this.Controls.Add(this.Khmelnychyi_KN_31);
             this.Controls.Add(this.KN_11INT_3);
             this.Controls.Add(this.kunderenko_KN31_5);
@@ -97,6 +109,7 @@
         private System.Windows.Forms.Button kunderenko_KN31_5;
         private System.Windows.Forms.Button KN_11INT_3;
         private System.Windows.Forms.Button Khmelnychyi_KN_31;
+        private System.Windows.Forms.Button karpov_kn_31;
     }
 }
 
